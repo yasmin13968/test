fe7a810ebea0c807ee91ade657946dc2769898c4
Yasmin <Yasmin@DESKTOP-HL49VNM>
2022-05-09 21:38:39 +0530

    First Commit

---
+++ testproject/src/testproject/add2numbers.java
@@ -0,0 +1,15 @@
+package testproject;
+
+public class add2numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		int sum = a + b;
+		System.out.println(sum);
+
+
+	}
+
+}
---
+++ testproject/src/testproject/helloworld.java
@@ -0,0 +1,10 @@
+package testproject;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
